fc57a4a8ba2656b4c1d3b91ae8d21bd67970954b
Martin Mullins <martin.mullins942@gmail.com>
2017-06-30 18:21:37 -0700

    move c50b to the root

---
+++ c50b/exp.h
@@ -0,0 +1,7 @@
+#ifndef __EXP_H
+#define __EXP_H
+
+extern int port_to_use;
+
+#endif
+
---
+++ c50b/func.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <linux/if_packet.h>
+#include <pthread.h>
+#include <linux/sched.h>
+#include <netinet/tcp.h>
+#include <sys/syscall.h>
+#include <signal.h>
+#include <sched.h>
+#include <sys/utsname.h>
+#include <pthread.h>
+#include <dlfcn.h>
+#include "func.h"
+#include "mod.h"
+#include "nop.h"
+#include "deviceconfig.h"
+
+#define LOLSTR "elliptical %p\n"
+const char* lol = LOLSTR;
+
+PhyPointer copyfunc(PhyPointer p) {
+    struct timer_list l;
+    l.next = 0;
+    l.prev = 0;
+    l.expires = 10;
+    l.base = 11;
+    l.function = (void*)p.p+3072; //(void*)0x0000f0ba;
+    l.data = 0xe;
+    l.slack = 15;
+
+    printf("Executing copy func...");
+    void* result = memcpy(p.v, (const void*)mod_func, mod_func_len);
+    printf("\t%p == %p\n",result, p.v);
+
+    result = memcpy(p.v+3072, (const void*)nop_func, nop_func_len);
+
+    result = memcpy(((void*)(((unsigned long)p.v)+1024)), (const void*)lol, sizeof(LOLSTR));
+    printf("\t%p == %p\n",result, ((void*)(((unsigned long)p.v)+1024)));
+    printf(((char*)(((unsigned long)p.v)+1024)));
+    result = memcpy(((void*)(((unsigned long)p.v)+2048)), (const void*)&l, sizeof(struct timer_list));
+
+    return p;
+}
